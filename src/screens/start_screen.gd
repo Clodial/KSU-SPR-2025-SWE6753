976852f482eae1efa3f6f7e219a7edad07e46c2f
Clodial <maravillamatthew@gmail.com>
2025-03-03 01:26:55 -0500

    added some emitters to start screen

--- src/screens/start_screen.gd
+++ src/screens/start_screen.gd
@@ -1,5 +1,8 @@
 extends Node
 
+signal start_game
+signal exit_game
+signal continue_game
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,3 +12,15 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_start_game_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_exit_game_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_continue_game_button_pressed() -> void:
+	pass # Replace with function body.
