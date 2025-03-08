a7043aa70f673cb503249e875bfde4cb8f899946
Clodial <maravillamatthew@gmail.com>
2025-03-08 16:36:53 -0500

    working level 4 connection

--- src/screens/level_select.gd
+++ src/screens/level_select.gd
@@ -10,7 +10,13 @@ signal select_level_2
 @export var level_2_text: String
 @export var level_2_active: bool
 signal select_level_3
+@export var level_3: PackedScene
+@export var level_3_text: String
+@export var level_3_active: bool
 signal select_level_4
+@export var level_4: PackedScene
+@export var level_4_text: String
+@export var level_4_active: bool
 signal select_level_5
 
 # Called when the node enters the scene tree for the first time.
@@ -37,5 +43,21 @@ func _on_level_button_2_pressed() -> void:
 func _on_level_button_2_mouse_exited() -> void:
 	$sub_level_label.text = ""
 
+func _on_level_button_3_mouse_entered() -> void:
+	$sub_level_label.text = level_3_text
+func _on_level_button_3_pressed() -> void:
+	if(level_3_active):
+		select_level_3.emit()
+func _on_level_button_3_mouse_exited() -> void:
+	$sub_level_label.text = ""
+
+func _on_level_button_4_mouse_entered() -> void:
+	$sub_level_label.text = level_4_text
+func _on_level_button_4_pressed() -> void:
+	if(level_4_active):
+		select_level_4.emit()
+func _on_level_button_4_mouse_exited() -> void:
+	$sub_level_label.text = ""
+	
 func _on_main_menu_button_button_down() -> void:
 	back_to_main.emit()
