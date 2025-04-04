2b9c6c0678e6f0cc364e4f39b326103b19357098
Matthew Maravilla <matthewmaravilla@Matthews-Laptop.local>
2025-04-03 23:24:41 -0400

    working level saving and unlocking

--- src/screens/level_select.gd
+++ src/screens/level_select.gd
@@ -1,73 +1,92 @@
 extends Node
 
+var level_data
+
 signal back_to_main
-signal select_level_1
-@export var level_1: PackedScene
-@export var level_1_text: String
-@export var level_1_active: bool
-signal select_level_2
-@export var level_2: PackedScene
-@export var level_2_text: String
-@export var level_2_active: bool
-signal select_level_3
-@export var level_3: PackedScene
-@export var level_3_text: String
-@export var level_3_active: bool
-signal select_level_4
-@export var level_4: PackedScene
-@export var level_4_text: String
-@export var level_4_active: bool
-signal select_level_5
-@export var level_5: PackedScene
-@export var level_5_text: String
-@export var level_5_active: bool
+signal select_level1_1
+@export var level1_1: PackedScene
+var level1_1_code = "level1_1"
+@export var level1_1_text: String
+@export var level1_1_active: bool
+signal select_level1_2
+@export var level1_2: PackedScene
+var level1_2_code = "level1_2"
+@export var level1_2_text: String
+@export var level1_2_active: bool
+signal select_level1_3
+@export var level1_3: PackedScene
+var level1_3_code = "level1_3"
+@export var level1_3_text: String
+@export var level1_3_active: bool
+signal select_level1_4
+@export var level1_4: PackedScene
+var level1_4_code = "level1_4"
+@export var level1_4_text: String
+@export var level1_4_active: bool
+signal select_level1_5
+@export var level1_5: PackedScene
+var level1_5_code = "level1_5"
+@export var level1_5_text: String
+@export var level1_5_active: bool
+var available_levels = [
+	level1_1_code,
+	level1_2_code,
+	level1_3_code,
+	level1_4_code,
+	level1_5_code
+]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$main_level_label.text = "Sprint 1 demo levels"
 	$sub_level_label.text = ""
+	
+
+func game_data_set(data) -> void:
+	level_data = data
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+	
 
 func _on_level_button_mouse_entered() -> void:
-	$sub_level_label.text = level_1_text
+	$sub_level_label.text = level1_1_text
 func _on_level_button_button_down() -> void:
-	if(level_1_active):
-		select_level_1.emit()
+	if(level_data.get("available_levels").has(level1_1_code)):
+		select_level1_1.emit()
 func _on_level_button_mouse_exited() -> void:
 	$sub_level_label.text = ""
 
 func _on_level_button_2_mouse_entered() -> void:
-	$sub_level_label.text = level_2_text
+	$sub_level_label.text = level1_2_text
 func _on_level_button_2_pressed() -> void:
-	if(level_2_active):
-		select_level_2.emit()
+	if(level_data.get("available_levels").has(level1_2_code)):
+		select_level1_2.emit()
 func _on_level_button_2_mouse_exited() -> void:
 	$sub_level_label.text = ""
 
 func _on_level_button_3_mouse_entered() -> void:
-	$sub_level_label.text = level_3_text
+	$sub_level_label.text = level1_3_text
 func _on_level_button_3_pressed() -> void:
-	if(level_3_active):
-		select_level_3.emit()
+	if(level_data.get("available_levels").has(level1_3_code)):
+		select_level1_3.emit()
 func _on_level_button_3_mouse_exited() -> void:
 	$sub_level_label.text = ""
 
 func _on_level_button_4_mouse_entered() -> void:
-	$sub_level_label.text = level_4_text
+	$sub_level_label.text = level1_4_text
 func _on_level_button_4_pressed() -> void:
-	if(level_4_active):
-		select_level_4.emit()
+	if(level_data.get("available_levels").has(level1_4_code)):
+		select_level1_4.emit()
 func _on_level_button_4_mouse_exited() -> void:
 	$sub_level_label.text = ""
 	
 func _on_level_button_5_pressed() -> void:
-	if(level_5_active):
-		select_level_5.emit()
+	if(level_data.get("available_levels").has(level1_5_code)):
+		select_level1_5.emit()
 func _on_level_button_5_mouse_entered() -> void:
-	$sub_level_label.text = level_5_text
+	$sub_level_label.text = level1_5_text
 func _on_level_button_5_mouse_exited() -> void:
 	$sub_level_label.text = ""
 	
