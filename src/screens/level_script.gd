f0fcc14746c8084f347e21027fbfc914e0190477
alexfang452 <alexfang452@gmail.com>
2025-04-05 12:41:52 -0400

    lives updated (thanks to Matthew)

--- src/screens/level_script.gd
+++ src/screens/level_script.gd
@@ -4,20 +4,23 @@ signal level_win
 signal level_lose
 var player_goal
 var level_finish
-var lives = 3
+var lives
+var level_code
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	lives = 3;
 	player_goal = 0
 	level_finish = false;
 	$Goal_P1.player_touch.connect(self._goal_touch.bind())
 	$Goal_P2.player_touch.connect(self._goal_touch.bind())
 	$Goal_P1.player_out.connect(self._goal_leave.bind())
 	$Goal_P2.player_out.connect(self._goal_leave.bind())
-	$Player.enemy_collision.connect(self._process_level_lose.bind())
-	$Player2.enemy_collision.connect(self._process_level_lose.bind())
+	$Player.enemy_collision.connect(
+		self._process_level_lose.bind($Player, $Player2, $Player1Marker, $Player2Marker))
+	$Player2.enemy_collision.connect(
+		self._process_level_lose.bind($Player, $Player2, $Player1Marker, $Player2Marker))
 	process_mode = Node.PROCESS_MODE_PAUSABLE
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -31,5 +34,10 @@ func _goal_touch() -> void:
 func _goal_leave() -> void:
 	player_goal -= 1;
 	
-func _process_level_lose() -> void:
-	level_lose.emit()
+func _process_level_lose(player1, player2, player1Marker, player2Marker) -> void:
+	if(lives > 1):
+		lives = lives - 1;
+		player1.position = player1Marker.position
+		player2.position = player2Marker.position
+	else:
+		level_lose.emit()
--- src/utils/PlayerProgress.gd
+++ src/utils/PlayerProgress.gd
@@ -7,6 +7,7 @@ var main_progress
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	main_progress = load_game()
+	print(OS.get_data_dir())
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -19,7 +20,7 @@ func new_game():
 		"section_2_health": max_health,
 		"section_3_health": max_health, 
 		"available_levels": [
-			"level1-1"
+			"level1_1"
 		]
 	}
 	return game_progress
@@ -28,14 +29,22 @@ func get_progress():
 	return main_progress
 	
 func set_progress_add_level(level_array):
-	main_progress.set("available_levels", level_array)
+	main_progress["available_levels"] = level_array;
+	save_game();
 	return main_progress
 	
 
 func save_game() -> void:
+	print("hi");
 	var save_file = FileAccess.open(SAVE_PATH, FileAccess.WRITE)
 	var save_dict = main_progress
 	save_file.store_line(JSON.stringify(save_dict))
+	
+func check_has_save():
+	if not FileAccess.file_exists(SAVE_PATH):
+		return false;
+	return true;
+	
 	
 func load_game():
 	if not FileAccess.file_exists(SAVE_PATH):
@@ -44,4 +53,12 @@ func load_game():
 	var json = JSON.new()
 	json.parse(load_file.get_line())
 	var save_dict = json.get_data() as Dictionary
+	print(save_dict);
 	return save_dict
+
+func create_new_game_file():
+	if FileAccess.file_exists(SAVE_PATH):
+		print("hello")
+		DirAccess.remove_absolute(SAVE_PATH);
+		return true;
+	return false;
